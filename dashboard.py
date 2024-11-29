aab6aae0798cda3ca251972a59fd78375398bc99
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-29 10:55:42 +0700

    Update UI

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ st.markdown(
     }}
     h1 {{
         font-family: 'Cabo Rounded', sans-serif;
-        font-size: 25px;
+        font-size: 45px;
         color: #FFFFFF;
         text-align: center;
     }}
@@ -32,8 +32,8 @@ with app1:
         """
         <a href='https://docs.google.com/spreadsheets/d/1n8HHYmRPGFZH21bLVBkb13jxhWEwIpVQJZ0bO529gQ0/edit?gid=0#gid=0' 
            target='_blank' style='text-decoration:none;'>
-            <div style='background-color:#00B2D6; padding:20px; text-align:center; border-radius:10px; color:#222831; 
-                        font-family:Cabo Rounded,sans-serif; font-size:16px; border:3px solid black;'>
+            <div style='background-color:#00B2D6; padding:25px; text-align:center; border-radius:10px; color:#222831; 
+                        font-family:Cabo Rounded,sans-serif; font-size:20px; border:4px solid black;'>
                 <strong style='color:white;'>Analyst Jobdesc Recap</strong>
             </div>
         </a>
@@ -49,8 +49,8 @@ with app2:
         """
         <a href='https://docs.google.com/spreadsheets/d/15LL3oA7cfSFSHNWCjKLrRCywtNsaCsDqO0GSnHVX63Q/edit?gid=0#gid=0' 
            target='_blank' style='text-decoration:none;'>
-            <div style='background-color:#00B2D6; padding:20px; text-align:center; border-radius:10px; color:#222831; 
-                        font-family:Cabo Rounded,sans-serif; font-size:16px; border:3px solid black;'>
+            <div style='background-color:#00B2D6; padding:25px; text-align:center; border-radius:10px; color:#222831; 
+                        font-family:Cabo Rounded,sans-serif; font-size:20px; border:4px solid black;'>
                 <strong style='color:white;'>Req CN to Validasi BA</strong>
             </div>
         </a>
