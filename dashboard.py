1ae424e4b187033547c424f04ef876cfbd7d8b84
Analyst-FPnA <analyst.fpna@gmail.com>
2024-11-29 09:35:44 +0700

    Test UI

--- dashboard.py
+++ dashboard.py
@@ -35,7 +35,7 @@ with app1:
         <a href='https://docs.google.com/spreadsheets/d/1n8HHYmRPGFZH21bLVBkb13jxhWEwIpVQJZ0bO529gQ0/edit?gid=0#gid=0' 
            target='_blank' style='text-decoration:none;'>
             <div style='background-color:#00B2D6; padding:20px; text-align:center; border-radius:10px; color:#222831; 
-                        font-family:Cabo-Rounded,sans-serif; font-size:16px;'>
+                        font-family:Cabo-Rounded,sans-serif; font-size:16px; border:2px solid black;'>
                 <strong style='color:white;'>Analyst Jobdesc Recap</strong>
             </div>
         </a>
